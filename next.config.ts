0a29373d8230fac148facba7ee7b6e70c28d44bc
Akash Singh <manduakash@gmail.com>
2026-03-12 15:17:57 +0530

    akash

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
   typescript: {
     ignoreBuildErrors: true,
   },
 
   async redirects() {
-    return [];
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: false,
+      },
+    ];
   },
 };
 
