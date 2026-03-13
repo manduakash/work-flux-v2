b15b411eb3065f42c680f20194601f2b9d2e8822
Akash Singh <manduakash@gmail.com>
2026-03-13 11:10:52 +0530

    akash

--- src/app/create-manage-task/page.tsx
+++ src/app/create-manage-task/page.tsx
@@ -12,6 +12,18 @@ import {
     CircleX
 } from 'lucide-react';
 import { toast } from 'sonner';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Label } from "@/components/ui/label"
 
 import { useStore } from "@/store/useStore";
 import { Button } from '@/components/ui/button';
@@ -1122,6 +1134,40 @@ export default function TaskManagementPage() {
                     </div>
                 )}
             </AnimatePresence>
+
+            {/* rejection modal */}
+             <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline">Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-sm">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </Field>
+            <Field>
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
         </div>
     );
 }
